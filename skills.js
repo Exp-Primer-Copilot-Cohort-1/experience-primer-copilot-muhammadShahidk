f362de182368fb55daf8df6550339594565b2e75
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2024-01-31 09:27:45 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,14 @@
+function calculateNumbers(var1, var2)
+{
+    var sum = var1 + var2;
+    var product = var1 * var2;
+    var difference = var1 - var2;
+    var quotient = var1 / var2;
+
+    return [sum, product, difference, quotient];
+}
+
+console.log(calculateNumbers(10, 5));
+
+
+
